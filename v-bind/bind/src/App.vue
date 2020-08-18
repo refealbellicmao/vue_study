90524600a2846e2714dfa58dcf546b74d4f7ab35
refeal <398552004@qq.com>
2020-08-18 22:15:27 +0800

    first commit

---
+++ v-bind/bind/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h2>{{message}}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+const app = new Vue({
+  el:'#app',
+  data:{
+    message:'hello'
+  }
+})
+</script>
+
+<style>
+
+</style>
